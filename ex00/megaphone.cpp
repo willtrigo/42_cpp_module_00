5f6f0d68baf59d027e42db2e4887024861869c29
dande-je <dande-je@student.42sp.org.br>
2025-01-13 20:03:49 -0300

    Feat: Create megaphone.  Changes to be committed: 	modified:   megaphone.cpp

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,34 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 12:48:24 by dande-je          #+#    #+#             */
-/*   Updated: 2025/01/08 22:27:07 by dande-je         ###   ########.fr       */
+/*   Updated: 2025/01/13 20:03:07 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <cstdlib>
+#include <iostream>
+
+static std::string megaphone(int argc, char** argv);
+
+int main(int argc, char** argv) {
+	std::cout << megaphone(--argc, ++argv) << std::endl;
+	return EXIT_SUCCESS;
+}
+
+static std::string megaphone(int argc, char** argv) {
+	std::string str;
+
+	if (argc)
+	{
+		while (*argv)
+		{
+			char* chr = *argv;
+			while (*chr)
+				str += std::toupper(*chr++);
+			argv++;
+		}
+	}
+	else
+		str = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	return str;
+}
